1f36a33f52fcc516670dfb0a17a45681058796d5
BulNim <114121809+BulNim@users.noreply.github.com>
2022-09-30 14:13:34 +0900

    week4
    week4

---
+++ week4/FedScale/examples/notebook/fedscale_demo_server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Federated Learning for Image Classification using Fedscale
+
+# ## Server Side
+
+# In[1]:
+
+
+import sys, os
+
+from fedscale.core.execution.client import Client
+from fedscale.core.aggregation.aggregator import Aggregator
+from fedscale.core.logger.execution import args
+Demo_Aggregator = Aggregator(args)
+### On CPU
+args.use_cuda = "False"
+Demo_Aggregator.run()
+
+
+# In[ ]:
+
+
+get_ipython().system('tensorboard --logdir=./logs/demo_job --port=6007 --bind_all')
+
